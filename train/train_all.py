b309f473eefd88707e53f7258cf805debc68b939
Patvath88 <patvath8@gmail.com>
2025-11-20 13:41:51 -0500

    Update train_all.py

--- train/train_all.py
+++ train/train_all.py
@@ -1,18 +1,20 @@
 # train/train_all.py
 
-from train_model import train_stat_model
+from train.train_model import train_stat_model
+
 
 def main():
-    print("Training POINTS model...")
+    print("Training POINTS...")
     train_stat_model("points")
 
-    print("\nTraining REBOUNDS model...")
+    print("\nTraining REBOUNDS...")
     train_stat_model("rebounds")
 
-    print("\nTraining ASSISTS model...")
+    print("\nTraining ASSISTS...")
     train_stat_model("assists")
 
     print("\nAll models trained successfully!")
+
 
 if __name__ == "__main__":
     main()
