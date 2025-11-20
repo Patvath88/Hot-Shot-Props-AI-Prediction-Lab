d9b026f5356e07bde1b17a2a6639306ea2def2e6
Patvath88 <patvath8@gmail.com>
2025-11-20 13:44:03 -0500

    Update train_all.py

--- train/train_all.py
+++ train/train_all.py
@@ -1,5 +1,15 @@
 # train/train_all.py
 
+import sys
+import os
+
+# ------------------------------------------------------------
+# FIX: Ensure this script can import train_model.py from repo root
+# ------------------------------------------------------------
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, ".."))
+sys.path.insert(0, ROOT_DIR)
+
 from train.train_model import train_stat_model
 
 
