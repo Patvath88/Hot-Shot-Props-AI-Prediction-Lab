ea36e64bec5eebb65e3ea36f4cff761ada8ae0f8
Patvath88 <patvath8@gmail.com>
2025-11-20 16:25:27 -0500

    Update train_all.py

--- train/train_all.py
+++ train/train_all.py
@@ -1,11 +1,12 @@
 from train.train_model import train_stat_model
 
 def main():
-    for stat in ["points", "rebounds", "assists"]:
-        train_stat_model(stat)
-
-    print("🎉 ALL MODELS TRAINED")
-
+    print("Training POINTS...")
+    train_stat_model("points")
+    print("Training REBOUNDS...")
+    train_stat_model("rebounds")
+    print("Training ASSISTS...")
+    train_stat_model("assists")
 
 if __name__ == "__main__":
     main()
